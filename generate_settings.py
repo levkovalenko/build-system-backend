ede135e5c1d935c5d3d419c25d2e85f6ef8d8fe5
korwin <levozavr@mail.ru>
2019-02-07 16:02:58 +0300

    change build system

---
+++ generate_settings.py
@@ -0,0 +1,30 @@
+import os
+
+
+def get_var(key: str) -> str:
+    result = os.environ.get(key)
+    if result is None:
+        raise Exception(f'Where is no {key} in os environ.')
+    else:
+        return result
+
+
+with open("./backend/local_settings.py", 'w') as f:
+    f.write(f"""
+DEBUG = True
+
+DATABASES = {{
+    'default': {{
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '{get_var("BASE_NAME")}',
+        'USER': '{get_var("USER_NAME")}',
+        'PASSWORD': '{get_var("BASE_PASSWORD")}',
+        'HOST': '{get_var("BASE_HOST")}',
+        'PORT': '{get_var("BASE_PORT")}',
+    }}
+}}
+
+BROKER_URL = "{get_var("BROKER_URL")}"
+
+ALLOWED_HOSTS = ['*']
+""")
